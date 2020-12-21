288192065263df8f165846c4c16d4ab1620771f6
Nate Voisan <rufio96@gmail.com>
2020-12-21 14:47:26 -0800

    commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://stark-beyond-58110.herokuapp.com/api'
+  API_ENDPOINT: 'https://dry-reef-37103.herokuapp.com/api'
   // API_ENDPOINT: 'http://localhost:8000/api'
 }
